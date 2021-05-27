c97c2d7d24e5ea00290fe2a9048a4e42d107c5d6
mitch cutts <mitchellbcutts@gmail.com>
2021-05-27 09:21:55 -0600

    commit 2

--- polygon_experimentation.py
+++ polygon_experimentation.py
@@ -2,14 +2,20 @@ import open3d as o3d
 import numpy as np
 import pandas
 
-pcd = o3d.io.read_point_cloud("face.ply")
+pcd = o3d.io.read_point_cloud("./sample_data/face.ply")
 vis = o3d.visualization.VisualizerWithEditing() 
 vis.create_window()
 vis.add_geometry(pcd)
 print("to select vertices for your polygon, press shift + left click, press shift + right click to delete a selection\n")
+print("Demo for manual geometry cropping")
+print("1) Press 'Y' twice to align geometry with negative direction of y-axis")
+print("2) Press 'K' to lock screen and to switch to selection mode")
+print("3) Drag for rectangle selection,")
+print("   or use ctrl + left click for polygon selection")
+print("4) Press 'C' to get a selected geometry and to save it")
+print("5) Press 'F' to switch to freeview mode")
 vis.run()
 vis.destroy_window()
-print("points gathered...drawing lines....")
 
 picked = vis.get_picked_points() #these are the indices of the points in the point cloud.
 
@@ -31,17 +37,38 @@ for i in picked:
         index_2 += 1
 
 #TODO add testing to ensure polygon is legally formed before printing again (ie. solving crossing lines problem, zero index problems)
+#we will be handling complex polygons by turning them into two polygons
+print("points gathered...drawing lines....")
+
 
 indices.append([index_2, 0])
 linepoints.append(linepoints[0])
 colorslist = [[255,0,0] for i in range(len(indices))]
-print(indices)
-print(linepoints)
+
+
+bounding_polygon = np.asarray(o3d.utility.Vector3dVector(linepoints))
+
+vol = o3d.visualization.SelectionPolygonVolume()
+vol.orthogonal_axis = "Z"
+print(bounding_polygon[:, 2])
+vol.axis_max = np.max(bounding_polygon[:, 2])
+vol.axis_min = np.min(bounding_polygon[:, 2])
 
 
 lineSet = o3d.geometry.LineSet()
 lineSet.colors = o3d.utility.Vector3dVector(colorslist)
 lineSet.points = o3d.utility.Vector3dVector(linepoints)
 lineSet.lines = o3d.utility.Vector2iVector(indices)
-o3d.visualization.draw_geometries([pcd, lineSet])
+vol.bounding_polygon = o3d.utility.Vector3dVector(bounding_polygon)
 
+# Crop the point cloud using the Vector3dVector
+cropped_pcd = vol.crop_point_cloud(pcd)
+bounding_box = cropped_pcd.get_axis_aligned_bounding_box()
+bounding_box.color = (1, 0, 1)
+cropped_pcd.paint_uniform_color([0,0.3,0])
+# Draw the newly cropped PCD and bounding box
+o3d.visualization.draw_geometries([lineSet, cropped_pcd, bounding_box],
+                                  zoom=2,
+                                  front=[5, -2, 0.5],
+                                  lookat=[7.67473496, -3.24231903,  0.3062945],
+                                  up=[1.0, 0.0, 0.0])
